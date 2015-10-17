2216dea1f89a34bc2f51ce6bb1c6d780cddd66b9
James Hall <james31415@gmail.com>
2015-10-17 18:49:34 +0000

    Forgot screenrc in makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zshrc oh-my-zsh"     # list of files/folders to symlink in homedir
+files="screenrc vimrc zshrc"     # list of files/folders to symlink in homedir
 
 ##########
 
