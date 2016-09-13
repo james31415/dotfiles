00eb7001432e3acf2526c8215e24dc87adea8783
James Hall <james31415@gmail.com>
2016-09-13 12:32:54 -0400

    Remove sudo from script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -39,7 +39,7 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        sudo chsh -s $(which zsh)
+        chsh -s $(which zsh) $SUDO_USER
     fi
 else
     # If zsh isn't installed, get the platform of the current machine
@@ -47,11 +47,11 @@ else
     # If the platform is Linux, try an apt-get to install zsh and then recurse
     if [[ $platform == 'Linux' ]]; then
         if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
+            yum install zsh
             install_zsh
         fi
         if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
+            apt-get install zsh
             install_zsh
         fi
     # If the platform is OS X, tell the user to install zsh :)
